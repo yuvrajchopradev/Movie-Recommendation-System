27548910b35b66123a867dc4b087a5b9c3002900
Yuvraj Chopra <yuvrajchopra014@gmail.com>
2026-05-22 23:05:52 +0530

    update api base in streamlit app

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import requests
 import streamlit as st
+import os
 
 # =============================
 # CONFIG
 # =============================
-API_BASE = "https://movie-rec-466x.onrender.com" or "http://127.0.0.1:8000"
+API_BASE = os.getenv("API_BASE")
 TMDB_IMG = "https://image.tmdb.org/t/p/w500"
 
 st.set_page_config(page_title="Movie Recommender", page_icon="🎬", layout="wide")
